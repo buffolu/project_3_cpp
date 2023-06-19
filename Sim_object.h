65da6333b42577abaaaba2dd50789d425cc05d7f
igor <hmelik420@gmail.com>
2023-06-19 15:01:04 +0300

    a

--- Sim_object.h
+++ Sim_object.h
@@ -7,6 +7,15 @@
 
 
 class Sim_object {
+    std::string name;
+    std::pair<float,float> location;
+    std::string state;
+
+protected:
+    std::string getName() const noexcept;
+    std::string getLocation() const noexcept;
+    void update();
+    std::string broad_current_state() const noexcept;
 
 };
 
