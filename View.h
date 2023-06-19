5891736e632baebc4c600f0ae838f73d63754c03
dannightmare <dan.kusai.star.123234@gmail.com>
2023-06-19 16:09:53 +0300

    added geometry.h .cpp

--- View.h
+++ View.h
@@ -5,9 +5,26 @@
 #ifndef PROJECT_3_VIEW_H
 #define PROJECT_3_VIEW_H
 
+#include "Geometry.h"
+#include "Sim_object.h"
+#include <set>
+#include <pair>
 
 class View {
+	int scale;	// number of kilometers in square side
+	int size_x;	// size of terminal window
+	int size_y;
+	Point origin;
+	std::set<Sim_object> objects;
 
+	public:
+	// getters setters
+	std::pair<int, int> getSize();
+	void setSize(int size);
+
+	void update_location();
+
+	void draw();
 };
 
 
