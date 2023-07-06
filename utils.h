9066aa2a42acd6f6dedcd510e00453c964d0da31
Daniel Kusai <dan.kusai@gmail.com>
2023-07-06 19:20:56 +0300

    added the utils files from a previous project might shorten it

---
+++ utils.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class utils 
+{
+	public:		// static methods
+	static std::vector<std::string> split(const std::string& str, char delim);
+	// static int calculateMinutesBetweenDates(const std::string& date1, const std::string& date2);
+};
