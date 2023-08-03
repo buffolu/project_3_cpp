aa992e1bcc1c24cd78b5231d28c95500a548b9dc
Daniel Kusai <dan.kusai@gmail.com>
2023-08-04 00:33:20 +0300

    added destination_structure and formatted code

--- Knight.cpp
+++ Knight.cpp
@@ -7,72 +7,81 @@
 Knight::Knight(const std::string &name, Point position)
     : Agent(name, position, 10, -1) {}
 
-
 void Knight::update() {
     Agent::update();
-    if(getState() == ON_DUTY)
-    {
-        if(getLocation() == next_castle->getLocation()){ // continue the patrol to a new structure
-                if(getLocation() == home_castle->getLocation() && going_home) //knight reached the end
-                {
-                    stop();
-                    return;
-                }
-                next_castle = check_for_closest();// with visited and mystructres}
-                if(next_castle == home_castle) {going_home = true;} //knight is ending his patrol
+    if (getState() == ON_DUTY) {
+        if (getLocation() ==
+            destination_structure
+                ->getLocation()) { // continue the patrol to a new structure
+            if (getLocation() == home_castle->getLocation() &&
+                going_home) // knight reached the end
+            {
+                stop();
+                return;
+            }
+            destination_structure =
+                check_for_closest(); // with visited and mystructres}
+            if (destination_structure == home_castle) {
+                going_home = true;
+            } // knight is ending his patrol
 
-
-            setAngle(  Point ::getAngle(getLocation(), next_castle->getLocation())  ); //set a new direction(angle)
+            setAngle(Point ::getAngle(
+                getLocation(),
+                destination_structure
+                    ->getLocation())); // set a new direction(angle)
         }
-        setLocation(Point::advance(getLocation(),getSpeed(), getAngle())); //advance towards new direction
+        setLocation(
+            Point::advance(getLocation(), getSpeed(),
+                           getAngle())); // advance towards new direction
     }
-
-
-
-
 }
 
-void Knight::setOnPatrol(const shared_ptr<Structure>& structure_,const shared_ptr<list<shared_ptr<Structure>>>& structures) {
+void Knight::setOnPatrol(
+    const shared_ptr<Structure> &structure_,
+    const shared_ptr<list<shared_ptr<Structure>>> &structures) {
     setState(ON_DUTY);
     home_castle = structure_;
-    next_castle = home_castle;
-    setAngle(Point::getAngle(getLocation(),structure_->getLocation())); // angle of riding direction
+    destination_structure = home_castle;
+    setAngle(Point::getAngle(
+        getLocation(), structure_->getLocation())); // angle of riding direction
     myStructures = structures;
 }
 
 void Knight::broadcast_current_state() const noexcept {
     Agent::broadcast_current_state();
     if (getState() == ON_DUTY) {
-        std::cout << " patrolling around " <<
-                  next_castle->getName();
-
+        std::cout << " patrolling around " << destination_structure->getName();
     }
 }
 shared_ptr<Structure> Knight::check_for_closest() {
-        if (visited.size() == myStructures->size()) return nullptr;
+    if (visited.size() == myStructures->size())
+        return nullptr;
 
-        double min = DBL_MAX;
-        double tmp;
-        shared_ptr<Structure> structure;
-        for (const auto &obj: *myStructures) {
-            auto it = find(visited.begin(), visited.end(), obj->getName());
-            if (it == visited.end())   //this line means that this structure is not visited yet
+    double min = DBL_MAX;
+    double tmp;
+    shared_ptr<Structure> structure;
+    for (const auto &obj : *myStructures) {
+        auto it = find(visited.begin(), visited.end(), obj->getName());
+        if (it == visited.end()) // this line means that this structure is not
+                                 // visited yet
+        {
+            tmp = Point::distance(getLocation(), obj->getLocation());
+            if (tmp < min) {
+                min = tmp;
+                structure = obj;
+            } else if (tmp == min &&
+                       obj->getName().front() <
+                           structure->getName()
+                               .front()) // choose by alphabetic order
             {
-                tmp = Point::distance(getLocation(), obj->getLocation());
-                if (tmp < min) {
-                    min = tmp;
-                    structure = obj;
-                } else if (tmp == min && obj->getName().front() < structure->getName().front())   //choose by alphabetic order
-                {
-                    min = tmp;
-                    structure = obj;
-                }
+                min = tmp;
+                structure = obj;
             }
         }
-        visited.emplace_back(structure->getName());
-        return structure;
     }
-
+    visited.emplace_back(structure->getName());
+    return structure;
+}
 
 void Knight::stop() {
     Agent::stop();
@@ -90,5 +99,3 @@ void Knight::setCourse(double theta) {
     stop();
     Agent::setCourse(theta);
 }
-
-
